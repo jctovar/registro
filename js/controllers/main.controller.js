663968e6f2a28f2ea2ac73ed8b64e697f90e2e72
Juan Carlos Tovar Gomez <jctovar@ired.unam.mx>
2016-04-25 09:52:00 -0500

    update

--- js/controllers/main.controller.js
+++ js/controllers/main.controller.js
@@ -13,24 +13,33 @@ angular.module('main.controllers', [])
 })
 
 .controller('speakersCtrl', function ($scope, $route, $routeParams, $location, speakers) {
+      $scope.separator_title = 'Nuestros ponentes';
+      $scope.separator_subtitle = 'Directorio de ponentes y profesores';
+      
       var query = speakers.get(function() {
         $scope.speakers = query.speakers; 
       }); 
 })
 
 .controller('loginCtrl', function ($scope, $route, $routeParams, $location) {
-      
+      $scope.separator_title = 'Inicia sesión';
+      $scope.separator_subtitle = 'Ingresa al sitio';
 })
 
 .controller('signinCtrl', function ($scope, $route, $routeParams, $location) {
-      
+      $scope.separator_title = 'Registrate';
+      $scope.separator_subtitle = 'Rellena el siguiente formulario con tus datos.';
 })
 
 .controller('aboutCtrl', function ($scope, $route, $routeParams, $location) {
-      
+      $scope.separator_title = 'Aviso de privacidad';
+      $scope.separator_subtitle = 'Políticas del Sitio y Declaración de protección de datos';
 })
 
 .controller('creditsCtrl', function ($scope, $route, $routeParams, $location, credits) {
+      $scope.separator_title = 'Directorio';
+      $scope.separator_subtitle = 'Directorio universitario';
+      
       var query = credits.get(function() {
         $scope.credits = query.credits; 
       });
